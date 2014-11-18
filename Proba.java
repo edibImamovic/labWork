159881858e74c53985f877a5bf89872c78460c04
edibi <edib.imamovic@gmail.com>
2014-11-18 18:59:32 +0100

    Create Proba.java

---
+++ Proba.java
@@ -0,0 +1,18 @@
+
+public class vjezbaVedad {
+	public static void main(String[] args) {
+		int A=56, B=12, C=25, D=3, E=52, F=22, G=39, H=7888, I=369, J=88;
+
+		System.out.printf("%5d,%5d,%5d,%5d,%5d",A,B,C,D,E);
+		System.out.printf("%5d,%5d,%5d,%5d,%5d",F,G,H,I,J);
+		
+		System.out.print("Unesi doule broj       ");
+		Double broj = TextIO.getDouble();
+				System.out.printf("Broj je %f  \n", broj);
+		
+		
+		
+		
+	}
+}
+
