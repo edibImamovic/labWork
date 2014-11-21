fdb6f49c01219b67030158b734c672a8f4e38de1
edibi <edib.imamovic@gmail.com>
2014-11-21 15:08:50 +0100

    Create vjezbaPetlje.java

---
+++ vjezbaPetlje.java
@@ -0,0 +1,25 @@
+
+public class vjezbaParni {
+	public static void main(String[] args) {
+	
+		
+		System.out.println("Molim Vas da unesete prvi broj:");
+		int prviBroj = TextIO.getInt();
+		System.out.println("Molim Vas da unesete drugi broj:");
+		int drugiBroj = TextIO.getInt();
+		
+		int sum = 0;
+		
+		while (prviBroj <=drugiBroj){
+			sum = sum +prviBroj;
+			prviBroj++;
+			
+			
+		}
+			
+		System.out.println(sum);
+		}
+		
+				
+			}
+		
